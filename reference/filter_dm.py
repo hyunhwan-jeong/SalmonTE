853ace500304d1af2d39f4f6a1f940a43b8623c6
Hyun-Hwan Jeong <hyunhwaj@bcm.edu>
2017-08-16 17:19:57 -0500

    added clades of DM

---
+++ reference/filter_dm.py
@@ -0,0 +1,20 @@
+with open("dm_origin.fa", "r") as inp:
+    seqs = inp.read().strip().split(">")[1:]
+
+with open("clades.tsv", "r") as inp:
+    clades = {}
+    for row in inp:
+        row = row.strip().split("\t")
+        clades[row[0]] = row[1]
+
+with open("dm.fa", "w") as oup, open("dm.clades", "w") as oup2:
+    for s in seqs:
+        first_line = s.split("\n")[0].split("\t")
+        assert len(first_line) == 3
+        if first_line[-1] != "Drosophila melanogaster":
+            continue
+        if clades[first_line[1]] == "DNA transposon":
+            print(first_line)
+            continue
+        oup.write(">{}".format(s))
+        oup2.write("{},{}\n".format(first_line[0],clades[first_line[1]]))

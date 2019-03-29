a710b2bc51675adc552554b60cbb1f7d9d9ee003
Hyun-Hwan Jeong <hyunhwaj@bcm.edu>
2019-03-29 02:21:17 -0500

    added a test case, and it works

--- test/test_SalmonTE.py
+++ test/test_SalmonTE.py
@@ -8,6 +8,12 @@ logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 class MyTestCase(unittest.TestCase):
         #self.assertEqual(True, True)
 
+    def test_collecting_fastq_files_rrcutler(self):
+        # This is a test regarding the issue: https://github.com/LiuzLab/SalmonTE/issues/12#issuecomment-477872509
+        self.input_info = SalmonTE.collect_FASTQ_files(["example/SRR7290434_R1.fq", "example/SRR7290434_R2.fq"])        
+        self.assertEqual(self.input_info["paired"], True)
+        self.assertEqual(self.input_info["num_fastq"], 1)
+
 
     def test_collecting_fastq_files_single_end(self):
         self.input_info = SalmonTE.collect_FASTQ_files(["example/CTRL_1_R1.fastq", "example/TARDBP_1_R1.fastq"])
@@ -15,9 +21,10 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.input_info["num_fastq"], 2)
         self.assertEqual(set(["CTRL_1_R1.fastq", "TARDBP_1_R1.fastq"]), set(self.input_info["file_list"]))
 
+
     def test_run_salmon(self):
         def test_collecting_fastq_files_paired_ends(self):
-            self.input_info = SalmonTE.collect_FASTQ_files(["example"])
+            self.input_info = SalmonTE.collect_FASTQ_files(["example/*.fastq"])
             self.assertEqual(self.input_info["paired"], True)
             self.assertEqual(self.input_info["num_fastq"], 4)
 
